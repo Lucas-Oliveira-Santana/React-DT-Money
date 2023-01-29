b1c95af13009801f4d78b467e7b1bfd6543aabe1
Lucas Oliveira <lucasoliveirasantana.dev@gmail.com>
2023-01-29 20:45:54 -0300

    :tada: DT Money first module

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import logoImg from "../../assets/Logo.svg"
+import { NewTransactionModal } from "../NewTransactionModal";
+import * as Dialog from '@radix-ui/react-dialog';
+
+export function Header(){
+  return(
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logoImg} />
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal/>
+        </Dialog.Root>
+      </HeaderContent>
+      </HeaderContainer>
+  )
+}
